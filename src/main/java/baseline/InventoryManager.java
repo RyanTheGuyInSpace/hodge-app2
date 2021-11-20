9f692a9cece311e7d6843e881570f2f206ba903b
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-11-20 01:06:12 -0500

    Big fat update

--- src/main/java/baseline/InventoryManager.java
+++ src/main/java/baseline/InventoryManager.java
@@ -5,6 +5,19 @@
 
 package baseline;
 
+import com.google.gson.Gson;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class InventoryManager {
 
     /**
@@ -18,9 +31,113 @@ public class InventoryManager {
     /**
      * Loads an existing inventory from a file.
      * @param path The path to the pre-existing Inventory.
-     * @return The loaded Inventory.
+     * @param items The ObservableList of InventoryItems to put the read InventoryItem into.
+     *
+     * -----
+     * Use Gson to read the provided path for InventoryItems
+     * Set the items list to the new InventoryItems that get read in
+     * -----
      */
-    public static Inventory loadInventory(String path) {
-        return new Inventory();
+    public static void loadInventory(String path, ObservableList<InventoryItem> items) {
+        Gson gson = new Gson();
+
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(path));
+
+            InventoryItem[] newItems = gson.fromJson(reader, InventoryItem[].class);
+
+            items.setAll(newItems);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    /**
+     * Writes the provided list of items into the specified destinationFile as JSON.
+     * @param items The list of InventoryItems to write to JSON.
+     * @param destinationFile The file to write the JSON into.
+     */
+    public static File exportToJSON(List<InventoryItem> items, File destinationFile) {
+        Gson gson = new Gson();
+
+        try {
+            FileWriter writer = new FileWriter(destinationFile.getPath());
+
+            writer.write(gson.toJson(items));
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+
+        }
+
+        return destinationFile;
+    }
+
+    /**
+     * Writes the provided list of items into the specified destinationFile as an HTML file
+     * where the items are contained inside a table element.
+     * @param items The list of InventoryItems to write to HTML.
+     * @param destinationFile The file to write the HTML into.
+     */
+    public static File exportToHTML(List<InventoryItem> items, File destinationFile) {
+        try {
+            FileWriter writer = new FileWriter(destinationFile.getPath());
+
+            writer.write("<!DOCTYPE html>");
+            writer.write("<HTML>");
+            writer.write("<head>");
+            writer.write(String.format("<title>%s</title>", destinationFile.getName()));
+            writer.write("</head>");
+            writer.write("<body>");
+            writer.write("<table>");
+            writer.write("<tr>");
+            writer.write("<th>Serial Number</th>\n" +
+                    "<th>Name</th>\n" +
+                    "<th>Value</th>\n");
+            writer.write("</tr>");
+
+            for (InventoryItem item : items) {
+                writer.write("<tr>");
+                writer.write(String.format("<td>%s</td>", item.getSerialNumber()));
+                writer.write(String.format("<td>%s</td>", item.getName()));
+                writer.write(String.format("<td>%s</td>", item.getValue()));
+                writer.write("</tr>");
+            }
+
+            writer.write("</table>");
+            writer.write("</body>");
+            writer.write("</HTML");
+
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+
+        }
+
+        return destinationFile;
+    }
+
+    /**
+     * Writes the provided list of items into the specified destinationFile as a TSV file.
+     * @param items The list of InventoryItems to write to TSV.
+     * @param destinationFile The file to write the TSV into.
+     */
+    public static File exportToTSV(List<InventoryItem> items, File destinationFile) {
+        try {
+            FileWriter writer = new FileWriter(destinationFile.getPath());
+
+            writer.write(String.format("Serial Number\tName\tValue\n"));
+
+            for (InventoryItem item : items) {
+                writer.write(String.format("%s\t%s\t%.2f\n", item.getSerialNumber(), item.getName(), item.getValue()));
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+
+        }
+
+        return destinationFile;
+    }
+
 }
--- src/main/java/baseline/InventoryManagerController.java
+++ src/main/java/baseline/InventoryManagerController.java
@@ -17,9 +17,13 @@ import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.converter.FloatStringConverter;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -29,6 +33,9 @@ import java.util.regex.Pattern;
 public class InventoryManagerController implements Initializable {
 
     ObservableList<InventoryItem> items = FXCollections.observableArrayList();
+
+    @FXML
+    public MenuItem exportButton;
 
     @FXML
     public TextField serialNumberAddField;
@@ -79,7 +86,7 @@ public class InventoryManagerController implements Initializable {
     public TableColumn<InventoryItem, String> serialNumColumn;
 
     @FXML
-    public TableColumn<InventoryItem, BigDecimal> valueColumn;
+    public TableColumn<InventoryItem, Float> valueColumn;
 
     @FXML
     public TableColumn<InventoryItem, String> nameColumn;
@@ -226,11 +233,91 @@ public class InventoryManagerController implements Initializable {
         items.remove(item);
     }
 
+    /**
+     * Opens a FileChooser to select the JSON file containing an inventory and
+     * loads it into the GUI.
+     *
+     * -----
+     * Open a FileChooser to select a JSON file
+     * Try to use Gson to read the JSON file into a list of InventoryItems
+     * If it fails, show an error message and return
+     * Otherwise, set the items list to contain all the items that were read in
+     * -----
+     */
+    public void openInventory() {
+        FileChooser fileChooser = new FileChooser();
+
+        fileChooser.setTitle("Select Inventory");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Json Files", "*.json"));
+
+        File loadFile = fileChooser.showOpenDialog(serialNumberAddField.getScene().getWindow());
+
+        InventoryManager.loadInventory(loadFile.getPath(), items);
+    }
+
+    /**
+     * Exports the currently listed inventory to a JSON file at a location specified with
+     * the FileChooser.
+     *
+     * -----
+     * Use the FileChooser to pick where the file should be exported to
+     *
+     * -----
+     */
+    public void exportInventory() {
+
+        FileChooser fileChooser = new FileChooser();
+
+        fileChooser.setTitle("Select Export Location");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("JSON File", "*.json"),
+                new FileChooser.ExtensionFilter("HTML File", "*.html"),
+                new FileChooser.ExtensionFilter("TSV File", "*.txt"));
+
+        File exportFile = fileChooser.showSaveDialog(serialNumberAddField.getScene().getWindow());
+
+        try {
+            exportFile.createNewFile();
+        } catch (IOException e) {
+            showError("Error exporting file. Please try again.");
+
+            return;
+        }
+
+        if (exportFile.getName().endsWith(".json")) {
+            // Export as JSON
+            InventoryManager.exportToJSON(items, exportFile);
+        } else if (exportFile.getName().endsWith(".html")) {
+            // Export as HTML
+            InventoryManager.exportToHTML(items, exportFile);
+        } else if (exportFile.getName().endsWith(".txt")) {
+            // Export as TSV
+            InventoryManager.exportToTSV(items, exportFile);
+        }
+    }
+
+    /**
+     * Exports the currently listed inventory to an HTML file at a location specified with
+     * the FileChooser.
+     */
+    public void exportToHTML() {
+
+    }
+
+    /**
+     * Exports the currently listed inventory to a TSV file at a location specified with
+     * the FileChooser.
+     */
+    public void exportToTSV() {
+
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         serialNumColumn.setCellValueFactory(new PropertyValueFactory<InventoryItem, String>("serialNumber"));
-        valueColumn.setCellValueFactory(new PropertyValueFactory<InventoryItem, BigDecimal>("value"));
+        valueColumn.setCellValueFactory(new PropertyValueFactory<InventoryItem, Float>("value"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<InventoryItem, String>("name"));
 
         InventoryItem item = new InventoryItem();
@@ -242,6 +329,10 @@ public class InventoryManagerController implements Initializable {
         items.add(item);
 
         itemsTable.setItems(items);
+
+        serialNumColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        valueColumn.setCellFactory(TextFieldTableCell.forTableColumn(new FloatStringConverter()));
+        nameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
 
         serialNumberAddField.setOnKeyPressed(event -> {
             if(event.getCode().equals(KeyCode.ENTER)) {
@@ -260,5 +351,82 @@ public class InventoryManagerController implements Initializable {
                 addItem();
             }
         });
+    }
+
+    /**
+     * Edits the serialNumber of the currently selected InventoryItem
+     * @param inventoryItemStringCellEditEvent The event being fired upon edit commit
+     */
+    public void editSerialNumber(TableColumn.CellEditEvent<InventoryItem, String> inventoryItemStringCellEditEvent) {
+        InventoryItem selectedItem = itemsTable.getSelectionModel().getSelectedItem();
+
+        String serialNumber = inventoryItemStringCellEditEvent.getNewValue();
+
+        if (serialNumber.length() > 13) {
+            showError("Not a valid serial number. Please try again.");
+
+            itemsTable.refresh();
+
+            return;
+        }
+
+        // Validate serialNumber with regex and make sure it doesn't already exist inside the items list
+        if (serialNumPattern.matcher(serialNumber).find()) {
+            // Check if this value is already inside the items list
+            for (InventoryItem item : items) {
+                if (item.getSerialNumber().equals(serialNumber)) {
+                    showError("Serial Number already exists. Please try again.");
+
+                    itemsTable.refresh();
+
+                    return;
+                }
+            }
+
+            selectedItem.setSerialNumber(serialNumber);
+
+            itemsTable.refresh();
+        } else {
+            showError("Not a valid serial number. Please try again.");
+
+            itemsTable.refresh();
+
+            return;
+        }
+    }
+
+    public void editValue(TableColumn.CellEditEvent<InventoryItem, Float> inventoryItemFloatCellEditEvent) {
+        float value;
+
+        try {
+            value = inventoryItemFloatCellEditEvent.getNewValue();
+
+            InventoryItem item = itemsTable.getSelectionModel().getSelectedItem();
+
+            item.setValue(value);
+        } catch (Exception e) {
+            // Show an error and exit
+            showError("Input for \"Value\" is invalid.");
+
+            return;
+        }
+
+        itemsTable.refresh();
+    }
+
+    public void editName(TableColumn.CellEditEvent<InventoryItem, String> inventoryItemStringCellEditEvent) {
+        InventoryItem item = itemsTable.getSelectionModel().getSelectedItem();
+
+        String name = inventoryItemStringCellEditEvent.getNewValue();
+
+        if (name.length() < 2 || name.length() > 256) {
+            showError("Name must be between 2 and 256 characters long.");
+
+            itemsTable.refresh();
+
+            return;
+        }
+
+        item.setName(name);
     }
 }
