468a7d27462a0ac270ced6376b0da56e25b3da2c
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-11-21 01:23:37 -0500

    Added test code

--- src/test/java/baseline/InventoryItemTest.java
+++ src/test/java/baseline/InventoryItemTest.java
@@ -8,25 +8,55 @@ class InventoryItemTest {
 
     @Test
     void getName() {
+        InventoryItem item = new InventoryItem();
+
+        item.setName("Item Name One");
+
+        assertEquals("Item Name One", item.getName());
     }
 
     @Test
     void setName() {
+        InventoryItem item = new InventoryItem();
+
+        item.setName("Set Item Name");
+
+        assertNotNull(item.getName());
     }
 
     @Test
     void getValue() {
+        InventoryItem item = new InventoryItem();
+
+        item.setValue((float)5.55);
+
+        assertEquals((float)5.55, item.getValue());
     }
 
     @Test
     void setValue() {
+        InventoryItem item = new InventoryItem();
+
+        item.setValue((float)15.55);
+
+        assertEquals((float)15.55, item.getValue());
     }
 
     @Test
     void getSerialNumber() {
+        InventoryItem item = new InventoryItem();
+
+        item.setSerialNumber("A-123-321-456");
+
+        assertEquals("A-123-321-456", item.getSerialNumber());
     }
 
     @Test
     void setSerialNumber() {
+        InventoryItem item = new InventoryItem();
+
+        item.setSerialNumber("A-123-321-ASD");
+
+        assertEquals("A-123-321-ASD", item.getSerialNumber());
     }
 }
--- src/test/java/baseline/InventoryManagerTest.java
+++ src/test/java/baseline/InventoryManagerTest.java
@@ -1,6 +1,10 @@
 package baseline;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,17 +12,73 @@ class InventoryManagerTest {
 
     @Test
     void loadInventory() {
+        ObservableList<InventoryItem> items = FXCollections.observableArrayList();
+
+        InventoryManager.loadInventory("inventory1.json", items);
+
+        assertNotEquals(0, items.size());
     }
 
     @Test
     void exportToJSON() {
+        ObservableList<InventoryItem> items = FXCollections.observableArrayList();
+
+        InventoryItem item = new InventoryItem();
+
+        item.setSerialNumber("A-123-123-123");
+        item.setValue((float)10.99);
+        item.setName("This is an item");
+
+        items.add(item);
+
+        File file = new File("testInventory.json");
+
+        InventoryManager.exportToJSON(items, file);
+
+        File file2 = new File("testInventory.json");
+
+        assertNotEquals(0, file2.length());
     }
 
     @Test
     void exportToHTML() {
+        ObservableList<InventoryItem> items = FXCollections.observableArrayList();
+
+        InventoryItem item = new InventoryItem();
+
+        item.setSerialNumber("A-123-123-123");
+        item.setValue((float)10.99);
+        item.setName("This is an item");
+
+        items.add(item);
+
+        File file = new File("testInventory.html");
+
+        InventoryManager.exportToJSON(items, file);
+
+        File file2 = new File("testInventory.html");
+
+        assertNotEquals(0, file2.length());
     }
 
     @Test
     void exportToTSV() {
+        ObservableList<InventoryItem> items = FXCollections.observableArrayList();
+
+        InventoryItem item = new InventoryItem();
+
+        item.setSerialNumber("A-123-123-123");
+        item.setValue((float)10.99);
+        item.setName("This is an item");
+
+        items.add(item);
+
+        File file = new File("testInventory.txt");
+
+        InventoryManager.exportToJSON(items, file);
+
+        File file2 = new File("testInventory.txt");
+
+        assertNotEquals(0, file2.length());
     }
 }
