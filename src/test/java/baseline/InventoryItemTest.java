bf7adf84d219ef075af59711703305caa29b6404
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-11-21 00:57:54 -0500

    Added empty test methods

---
+++ src/test/java/baseline/InventoryItemTest.java
@@ -0,0 +1,32 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryItemTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getValue() {
+    }
+
+    @Test
+    void setValue() {
+    }
+
+    @Test
+    void getSerialNumber() {
+    }
+
+    @Test
+    void setSerialNumber() {
+    }
+}
---
+++ src/test/java/baseline/InventoryManagerTest.java
@@ -0,0 +1,24 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryManagerTest {
+
+    @Test
+    void loadInventory() {
+    }
+
+    @Test
+    void exportToJSON() {
+    }
+
+    @Test
+    void exportToHTML() {
+    }
+
+    @Test
+    void exportToTSV() {
+    }
+}
